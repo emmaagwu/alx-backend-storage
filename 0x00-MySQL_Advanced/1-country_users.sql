9019db957b2eae3fb2a5ef243bd3026afc155ac8
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-11 13:44:38 +0100

    Add table 'users' with specified attributes

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Script to create a table named 'users' with specified attributes
+
+-- CREATE TABLE IF NOT EXISTS to ensure the script doesn't fail if the table already exists
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
