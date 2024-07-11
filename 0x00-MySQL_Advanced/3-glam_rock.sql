12343de8cad6afac30526b8fda4899762b5a2c76
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-11 14:54:40 +0100

    Refactor lifespan calculation in glam rock SQL query

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,8 +2,8 @@
 
 -- Select and calculate lifespan in years until 2022
 
-SELECT band_name,
-       IFNULL(split, 2022-2) - IFNULL(formed, 2022-2) AS lifespan
+SELECT band_name, 
+       (IFNULL(split, '2020') - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC, band_name ASC;
